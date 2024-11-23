bd6d932dfe67a6bef4650adca0eb143e8095c324
tim.billiet <tim.billiet@student.odisee.be>
2024-11-23 13:16:35 +0100

    stringtoint fix 3ms

--- String to Integer/src/Main.java
+++ String to Integer/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("int: " + myAtoi("0-1"));
+        System.out.println("int: " + myAtoi("  +  413"));//4193 with
     }
 
     public static int myAtoi(String s) {
@@ -8,15 +8,26 @@ public class Main {
         boolean tekenGelezen = false;
         boolean letterGedetecteerd = false;
         int aantalCijfers = 0;
+        int resultaat = 0;
+        char teken = 0;
         for (int i = 0; i < s.length(); i++) {
             char huidigChar = s.charAt(i);
             if (huidigChar != ' ') {
-                if ((huidigChar == '-' || huidigChar == '+') && !tekenGelezen) {
-                    if (aantalCijfers == 0) {
-                        resultString.append(huidigChar);
-                        tekenGelezen = true;
+                if ((huidigChar == '-' || huidigChar == '+')) {
+                    if (!tekenGelezen) {
+                        if (aantalCijfers == 0) {
+                            resultString.append(huidigChar);
+                            tekenGelezen = true;
+                            teken = huidigChar;
+                        } else {
+                            break;
+                        }
                     } else {
-                        return Integer.parseInt(resultString.toString());
+                        if(aantalCijfers > 0) {
+                            break;
+                        } else {
+                            return 0;
+                        }
                     }
                 } else if (huidigChar >= 'a' && huidigChar <= 'z') {
                     letterGedetecteerd = true;
@@ -26,9 +37,30 @@ public class Main {
                 } else if (huidigChar >= '0' && huidigChar <= '9' && !letterGedetecteerd) {
                     resultString.append(huidigChar);
                     aantalCijfers++;
+                } else if (huidigChar == '.') {
+                    if (aantalCijfers == 0) {
+                        return 0;
+                    } else {
+                        break;
+                    }
                 }
+                if(!tekenGelezen && i > 0) {
+                    teken = '+';
+                }
+            } else if(aantalCijfers > 0 || tekenGelezen){
+                break;
             }
         }
-        return Integer.parseInt(resultString.toString());
+        try {
+            resultaat = Integer.parseInt(resultString.toString());
+
+        } catch (NumberFormatException ex) {
+            if (teken == '-' && aantalCijfers > 1) {
+                return Integer.MIN_VALUE;
+            } else if (teken == '+' && aantalCijfers > 1) {
+                return Integer.MAX_VALUE;
+            }
+        }
+        return resultaat;
     }
 }
