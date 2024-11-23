ea5d23953e52e49c109e62b0a6bc6bd464813972
tim.billiet <tim.billiet@student.odisee.be>
2024-11-23 12:42:07 +0100

    stringtoint af

--- String to Integer/src/Main.java
+++ String to Integer/src/Main.java
@@ -4,42 +4,31 @@ public class Main {
     }
 
     public static int myAtoi(String s) {
-        int resultaat = 0;
         StringBuilder resultString = new StringBuilder();
-        boolean signRead = false;
-        boolean letterDetected = false;
-        int aantalWhite = 0;
+        boolean tekenGelezen = false;
+        boolean letterGedetecteerd = false;
         int aantalCijfers = 0;
         for (int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) != ' ') {
-
-                if ((s.charAt(i) == '-' || s.charAt(i) == '+') && !signRead) {
-                    signRead = true;
-                    if(aantalCijfers == 0) {
-                        resultString.append(s.charAt(i));
+            char huidigChar = s.charAt(i);
+            if (huidigChar != ' ') {
+                if ((huidigChar == '-' || huidigChar == '+') && !tekenGelezen) {
+                    if (aantalCijfers == 0) {
+                        resultString.append(huidigChar);
+                        tekenGelezen = true;
                     } else {
-                        System.out.println("happens");
                         return Integer.parseInt(resultString.toString());
-
                     }
-                }
-                if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
-                    letterDetected = true;
-                    if(aantalCijfers == 0) {
+                } else if (huidigChar >= 'a' && huidigChar <= 'z') {
+                    letterGedetecteerd = true;
+                    if (aantalCijfers == 0) {
                         return 0;
                     }
-                }
-                if(s.charAt(i) >= '0' && s.charAt(i) <= '9' && !letterDetected) {
-                    resultString.append(s.charAt(i));
+                } else if (huidigChar >= '0' && huidigChar <= '9' && !letterGedetecteerd) {
+                    resultString.append(huidigChar);
                     aantalCijfers++;
                 }
-
-            } else {
-                aantalWhite++;
             }
-
         }
-        System.out.println(resultString);
         return Integer.parseInt(resultString.toString());
     }
 }
