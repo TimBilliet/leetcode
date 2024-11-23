ae6e644e78238cd8d2c363d16b5a12b6aa02f711
Tim Billiet <tim.billiet@student.odisee.be>
2024-11-23 10:07:08 +0100

    stukje stringtoint

--- String to Integer/src/Main.java
+++ String to Integer/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("int: " + myAtoi("1337c0d3"));
+        System.out.println("int: " + myAtoi("0-1"));
     }
 
     public static int myAtoi(String s) {
@@ -8,20 +8,38 @@ public class Main {
         StringBuilder resultString = new StringBuilder();
         boolean signRead = false;
         boolean letterDetected = false;
+        int aantalWhite = 0;
+        int aantalCijfers = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '-' && s.charAt(i) == '+' && !signRead) {
-                resultString.append(s.charAt(i));
-                signRead = true;
+            if(s.charAt(i) != ' ') {
+
+                if ((s.charAt(i) == '-' || s.charAt(i) == '+') && !signRead) {
+                    signRead = true;
+                    if(aantalCijfers == 0) {
+                        resultString.append(s.charAt(i));
+                    } else {
+                        System.out.println("happens");
+                        return Integer.parseInt(resultString.toString());
+
+                    }
+                }
+                if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
+                    letterDetected = true;
+                    if(aantalCijfers == 0) {
+                        return 0;
+                    }
+                }
+                if(s.charAt(i) >= '0' && s.charAt(i) <= '9' && !letterDetected) {
+                    resultString.append(s.charAt(i));
+                    aantalCijfers++;
+                }
+
+            } else {
+                aantalWhite++;
             }
 
-            if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
-                letterDetected = true;
-            }
-            if (s.charAt(i) != ' ' && s.charAt(i) >= '0' && s.charAt(i) <= '9' && signRead) {
-               resultString.append(s.charAt(i));
-            }
         }
         System.out.println(resultString);
-        return resultaat;
+        return Integer.parseInt(resultString.toString());
     }
 }
